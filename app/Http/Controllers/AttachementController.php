21e1620a1bf4198886aa8815a748000f5422eb74
HadiHD17 <hadihaidar1723@gmail.com>
2025-07-17 22:56:19 +0300

    intialzed models for all tables

---
+++ app/Http/Controllers/AttachementController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Attachement;
+use App\Http\Requests\StoreAttachementRequest;
+use App\Http\Requests\UpdateAttachementRequest;
+
+class AttachementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAttachementRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Attachement $attachement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Attachement $attachement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAttachementRequest $request, Attachement $attachement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Attachement $attachement)
+    {
+        //
+    }
+}
--- database/migrations/2025_07_16_184214_create_capsules_table.php
+++ database/migrations/2025_07_16_184214_create_capsules_table.php
@@ -43,5 +43,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('capsules');
+        Schema::dropIfExists('attachements');
     }
 };
