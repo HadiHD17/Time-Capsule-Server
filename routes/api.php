48fdb6c944f4e9f504143629b7f0b906d3243a33
HadiHD17 <hadihaidar1723@gmail.com>
2025-07-18 14:13:27 +0300

    finished some routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CapsuleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(["prefix" => "v0.1"], function () {
+    Route::get("/capsules", [CapsuleController::class, "GetAllCapsules"]);
+    Route::get("get_capsule/{id}", [CapsuleController::class, "GetCapsuleById"]);
+    Route::post("add_capsule", [CapsuleController::class, "AddCapsule"]);
+    Route::post("login", [AuthController::class, "Login"]);
+    Route::post("register", [AuthController::class, "Register"]);
+});
